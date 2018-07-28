31dce7047eac408b5609d49212db9fedf5a8eb77
Grzegorz Skwarek <grzegorz.skwarek.95@gmail.com>
2018-07-28 17:24:36 +0200

    MovingPlatform and HasAuthority.

---
+++ Source/PuzzlePlatforms/MovingPlatform.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "MovingPlatform.h"
+
+AMovingPlatform::AMovingPlatform() {
+	PrimaryActorTick.bCanEverTick = true;
+
+	SetMobility(EComponentMobility::Movable);
+}
+
+void AMovingPlatform::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+
+	if (HasAuthority()) {
+		MovePlatfrom(DeltaTime);
+	}
+
+}
+
+void AMovingPlatform::MovePlatfrom(float DeltaTime)
+{
+	FVector Location = GetActorLocation();
+	Location += FVector(speed * DeltaTime, 0, 0);
+	SetActorLocation(Location);
+
+}
+
